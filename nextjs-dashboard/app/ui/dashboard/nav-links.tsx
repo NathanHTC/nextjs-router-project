4e907e3a4b47e7da17cfa5ab9b248c9a396969c0
nathan <000savior001@gmail.com>
2024-01-19 19:41:15 -0800

    comment added

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -33,6 +33,8 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={
+              //apply styles depends on pathname
+              //for pages user is on, render it blue in nav link 
               clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
               {
                 'bg-sky-100 text-blue-600': pathname === link.href,
