ab73bc15da392e172f0b2e0d350bb8c518c306c2
nathan <000savior001@gmail.com>
2024-02-23 22:19:30 -0800

    dashboard main page indentation adjusted, commented

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <div className={styles.shape}/>
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-100 px-6 py-10 md:w-2/5 md:px-20">
+          {/* <div className={styles.shape}/>  demo of using css module*/}
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
@@ -23,10 +23,18 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <Link href="/dashboard" className="flex bg-blue-400 rounded-lg text-white text-lg p-2 justify-center font-medium shrink border-black">DashBoard</Link>
+          <Link 
+            href="/dashboard" 
+            className="flex bg-blue-400 rounded-lg text-white text-lg p-2 
+            justify-center font-medium shrink border-black"
+          >
+            <span>DashBoard</span>
+          </Link>
+
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm
+            font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
