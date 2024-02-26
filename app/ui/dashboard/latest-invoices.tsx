1019f77a5661e3374a31c38675775c40dddf2a9c
nathan <000savior001@gmail.com>
2024-02-26 10:10:01 -0800

    Latest invoices changed to streaming component | via Suspense

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -4,12 +4,10 @@ import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import { LatestInvoice } from '@/app/lib/definitions';
 import { invoices } from '@/app/lib/placeholder-data';
+import { fetchLatestInvoices } from '@/app/lib/data';
 
-export default async function LatestInvoices({
-  latestInvoices,
-}: {
-  latestInvoices: LatestInvoice[];
-}) {
+export default async function LatestInvoices(){
+  const latestInvoices = await fetchLatestInvoices()
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-3xl`}>
