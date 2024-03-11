afc96cb38b550b60f6c9a24d7562b515fae598a0
nathan <000savior001@gmail.com>
2024-03-10 23:27:02 -0700

    extract data from increaseInvoice formData

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,5 +6,11 @@
 'use server'
 export async function createInvoice(formData: FormData){
     // console.log("createInvoice action function called")
-    
+    const rawFormData = {
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+      };
+      // Test it out:
+      console.log(rawFormData);
 }
