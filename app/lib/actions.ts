6ddc8e877afd4fa61254ec13d2dfa26d1308a5e5
nathan <000savior001@gmail.com>
2024-03-11 23:20:38 -0700

    action.js edited

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,11 +6,5 @@
 'use server'
 export async function createInvoice(formData: FormData){
     // console.log("createInvoice action function called")
-    const rawFormData = {
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-      };
-      // Test it out:
-      console.log(rawFormData);
+    
 }
