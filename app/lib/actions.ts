8306fa35e3040f3c45f8d0463e8fa38b4776e51b
nathan <000savior001@gmail.com>
2024-03-15 21:07:01 -0700

    action.js edited

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,20 @@
 //server functions are async and executed on server to 
 //handle form submission and data mutations
 'use server'
+
+
+import { z } from 'zod';
+ 
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+ 
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData){
     // console.log("createInvoice action function called")
     const rawFormData = {
