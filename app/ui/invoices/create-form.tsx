b22d99609d612d89ac9197e120131fd163441825
nathan <000savior001@gmail.com>
2024-03-24 12:43:50 -0700

    form stutus error displayed via form state hook | error msg are returned by zod

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -65,7 +65,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-                // required
+                required
                 aria-describedby='amount-error'
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -96,6 +96,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby='status-error'
                 />
                 <label
                   htmlFor="pending"
@@ -111,6 +112,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="paid"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby='status-error'
                 />
                 <label
                   htmlFor="paid"
@@ -120,8 +122,22 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 </label>
               </div>
             </div>
+            
           </div>
         </fieldset>
+        <div id='status-error' aria-live='polite' arai-atomic='true'>
+          {state.errors?.status && 
+              state.errors.status.map((error:string) => (
+                <p className='mt-2 text-sm text-red-500' key={error}>
+                  {error}
+                  <br></br>
+                  {state.message}
+                </p>
+                
+              ))
+            
+          }
+        </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
