5e35680c99c1049b93f1a59815b122833cae09bc
nathan <000savior001@gmail.com>
2024-03-23 20:46:24 -0700

    updateInvoice action server function added | update invoice and revalidate cache

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -45,3 +45,24 @@ export async function createInvoice(formData: FormData){
    //redirect user back to invoice page
    redirect('/dashboard/invoices');
 }
+
+const UpdateInvoice = FormSchema.omit({ id:true, date:true })
+
+export async function updateInvoice(id:string, formData: FormData){
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+    `
+    //purge client cache and redirect
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
